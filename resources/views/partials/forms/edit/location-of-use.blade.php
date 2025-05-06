3bf9971fa39452a2b9576cdbb65c9279116c6728
lam <nlam49084@gmail.com>
2025-05-06 14:28:23 +0700

    add location_of_use in bulk checkout, fix language

--- resources/views/partials/forms/edit/location-of-use.blade.php
+++ resources/views/partials/forms/edit/location-of-use.blade.php
@@ -2,7 +2,11 @@
 <div class="form-group {{ $errors->has('location_of_use') ? ' has-error' : '' }}">
     <label for="location_of_use" class="col-md-3 control-label">{{ $translated_location_of_use }}</label>
     <div class="col-md-7 col-sm-12">
-        <input class="form-control" style="width:100%;" type="text" name="location_of_use" placeholder="{{ trans('general.location_of_use_placeholder') }}" aria-label="location_of_use" id="location_of_use" value="{{ old('location_of_use', $item->location_of_use) }}"{!!  (Helper::checkIfRequired($item, 'location_of_use')) ? ' required' : '' !!} maxlength="191" />
+        <input class="form-control" style="width:100%;" type="text" name="location_of_use"
+               placeholder="{{ trans('general.location_of_use_placeholder') }}"
+               aria-label="location_of_use" id="location_of_use"
+               value="{{ old('location_of_use') }}"
+               maxlength="191" />
         {!! $errors->first('location_of_use', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
